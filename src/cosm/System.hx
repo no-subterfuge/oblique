4ec1e0806c479eff2dd39785e004c048e0f14402
loopingleaf <luc.lauriou@live.fr>
2021-06-02 23:43:51 +0200

    Add update

--- src/cosm/System.hx
+++ src/cosm/System.hx
@@ -1,6 +1,33 @@
 package cosm;
 
+import h2d.col.Point;
+
 class System
 {
-	public var celestials(default, null):Celestial;
+	public var celestials(default, null) : Array<Celestial>;
+
+	public function new()
+	{
+		celestials = [];
+	}
+
+	public function add(celestial : Celestial)
+	{
+		celestials.push(celestial);
+	}
+
+	public function update(dt : Float)
+	{
+		for (c1 in 0...celestials.length)
+		{
+			var totalGForce = new Point();
+			for (c2 in 0...celestials.length)
+			{
+				if (c1 == c2) // We don't need to apply the force on itself
+					continue;
+
+				totalGForce.add(celestials[c1].gForce(celestials[c2]));
+			}
+		}
+	}
 }
