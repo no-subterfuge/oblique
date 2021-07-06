18303318e0ed728b4e92c89c16ca4e0740e491b9
e-ol <luc.lauriou@live.fr>
2021-07-06 21:44:36 +0200

    Remove cosm import

--- src/Game.hx
+++ src/Game.hx
@@ -1,22 +1,21 @@
 import hxd.Window;
-import cosm.System;
 import h2d.col.Point;
 
 class Game extends hxd.App
 {
 	public static var INSTANCE(default, null) : Game;
 
-	public var systems : List<System>;
+	public var systems : List<cosm.System>;
 
 	override function init()
 	{
 		if (INSTANCE == null)
 			INSTANCE = this;
-		systems = new List<System>();
+		systems = new List<cosm.System>();
 		var cx = Window.getInstance().width / 2;
 		var cy = Window.getInstance().height / 2;
-		var sol = new System();
-		var earth = new cosm.Celestial(cx + 30., cy + 30., new Point(-10., 6.), 1.0);
+		var sol = new cosm.System();
+		var earth = new cosm.Celestial(cx + 10., cy + 10., new Point(-10., 6.), 1.0);
 		sol.add(earth);
 		var sun = new cosm.Celestial(cx, cy, new Point(0, 0), 4000.0);
 		sol.add(sun);
